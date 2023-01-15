becf28a2197c959d8bb1f9dfdc1d409c12ee4519
Dennis <dennis.dennis2012@gmail.com>
2023-01-15 20:26:11 -0300

    adicionando script

---
+++ script/script.js
@@ -0,0 +1,13 @@
+/* const screenMobile = document.querySelector(".mobile_container");
+
+
+
+let screenHeight = screenMobile.clientHeight;
+
+
+
+console.log(screenHeight+'px');
+
+
+
+screenMobile.style.height = screenHeight+"px"; */
