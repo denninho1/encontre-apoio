39080f33609ea09ae12a7e2f9d017969302aa7e8
Dennis <dennis.dennis2012@gmail.com>
2023-01-17 23:36:35 -0300

    side navigation interaction

--- script/script.js
+++ script/script.js
@@ -1,13 +1,24 @@
-/* const screenMobile = document.querySelector(".mobile_container");
+/* side navigation interaction */
+const buttonMenu = document.querySelector('.button_menu');
+const navSide = document.querySelector('.nav_side_container');
+const buttonClose = document.querySelector('.close_btn');
+const mobileContainer = document.querySelector('.mobile_container');
+
+const navShow = () => {
+    if(navSide.classList.contains('active')) {
+        navSide.classList.remove('active');
+        mobileContainer.style.overflowY = 'scroll'
+        return
+    }
+    navSide.classList.add('active');
+    mobileContainer.style.overflowY = 'hidden';
+}
+
+buttonMenu.addEventListener('click', () => {
+    navShow();
+});
+buttonClose.addEventListener('click', () => {
+    navShow();
+});
 
 
-
-let screenHeight = screenMobile.clientHeight;
-
-
-
-console.log(screenHeight+'px');
-
-
-
-screenMobile.style.height = screenHeight+"px"; */
