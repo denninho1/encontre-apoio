597d94778a58f2bc920c6468e7552554cf448214
Dennis <dennis.dennis2012@gmail.com>
2023-01-21 03:56:10 -0300

    mudança de tela ao clickar

--- script/script.js
+++ script/script.js
@@ -22,14 +22,29 @@ buttonClose.addEventListener('click', () => {
     navShow();
 });
 
-// nav bottom select
+// nav bottom select and change screen 
 const navBottomBtns = document.querySelectorAll('.nav_bottom_btn');
+const screens = document.querySelectorAll('.screen');
+const searchBtn = document.querySelector('.search_btn');
 
 navBottomBtns.forEach((navBtn, index) => {
     navBtn.addEventListener('click', () => {
         for (let i = 0; i < navBottomBtns.length; i++) {
             navBottomBtns[i].classList.remove('active');
         }
+        searchBtn.classList.remove('active')
+        if(navBtn.classList.contains('init')) {
+            screens[0].style.marginLeft = '0'
+        } else if(navBtn.classList.contains('meet')) {
+            screens[0].style.marginLeft = '-100%'
+            searchBtn.classList.add('active')
+        } else {
+            screens[0].style.marginLeft = '-200%'
+        }
+
         navBtn.classList.add('active');
     });
 });
+
+
+
